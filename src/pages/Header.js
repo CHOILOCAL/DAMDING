b2f80ac5eece24dc5ab073ae504e307dfc6ed4db
Choilocal <choihyunji1103@gmail.com>
2020-04-07 15:26:54 +0900

    footer

---
+++ src/pages/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from 'components/Login';
+import {
+    Container,
+    Divider,
+    Dropdown,
+    Grid,
+    Header,
+    Image,
+    List,
+    Menu,
+    Segment,
+} from 'semantic-ui-react'
+import logo from 'image/logo.png';
