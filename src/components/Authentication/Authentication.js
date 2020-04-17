bf6cebab7e34b439d6fffca93a521c951286fcb3
Choilocal <choihyunji1103@gmail.com>
2020-04-17 15:11:29 +0900

    login/signup form

--- src/components/Authentication/Authentication.js
+++ src/components/Authentication/Authentication.js
@@ -5,10 +5,18 @@ import {Link} from 'react-router-dom';
 
 import '../../css/Authentication.css';
 
-import { Form, Button } from 'react-bootstrap';
+import {Form, Col, Button} from 'react-bootstrap';
 
 class Authentication extends React.Component {
     render() {
+
+        // 회원가입, 로그인 뷰에서 보이는 중복 코드
+        const commonInputBox = (
+            <div className="input-field col s12 username">
+                <label>아이디</label>
+                <input name="username" type="text" className="validate" placeholder="아이디"/>
+            </div>
+        )
 
         // 로그인 뷰 <input name="username" type="text" className="validate"/> <input
         // name="password" type="password" className="validate"/> <a>로그인</a> <Link
@@ -17,31 +25,23 @@ class Authentication extends React.Component {
             <div>
                 <div className="card-content">
                     <div className="row">
-                        <div className="input-field col s12 username">
-                            <label>아이디</label>
-                            <input
-                            name="username"
-                            type="text"
-                            className="validate"
-                            placeholder="아이디" />
-                        </div>
+
+                        {/* 공통 <div /> */}
+                        {commonInputBox}
+
                         <div className="input-field col s12">
                             <label>비밀번호</label>
-                            <input
-                            name="password"
-                            type="password"
-                            className="validate"
-                            placeholder="비밀번호"/>
+                            <input name="password" type="password" className="validate" placeholder="비밀번호"/>
                         </div>
                         <a className="waves-effect waves-light btn">로그인</a>
                     </div>
                 </div>
 
-
                 <div className="footer">
                     <div className="card-content">
-                        <div className="right" >
-                        아이디 없음? <Link to="/signup">회원가입하러가기</Link>
+                        <div className="right">
+                            아이디 없음?
+                            <Link to="/signup">회원가입하러가기</Link>
                         </div>
                     </div>
                 </div>
@@ -49,7 +49,56 @@ class Authentication extends React.Component {
         );
 
         // 회원가입 뷰
-        const registerView = (<div>회원가입 화면</div>);
+        const registerView = (
+            <div>
+                <div className="card-content">
+                    <div className="row">
+
+                        {/* 공통 <div /> */}
+                        { commonInputBox }
+
+                        <div className="input-field col s12 username">
+                            <label>아이디</label>
+                            <input name="username" type="text" className="validate" placeholder="아이디 입력해"/>
+                        </div>
+                        <div className="input-field col s12">
+                            <label>비밀번호</label>
+                            <input name="password" type="text" className="validate" placeholder="패스워드 입력해"/>
+                        </div>
+                        <div className="input-field col s12">
+                            <label>비밀번호확인</label>
+                            <input
+                                name="passwordCheck"
+                                type="text"
+                                className="validate"
+                                placeholder="패스워드 또 입력해"/>
+                        </div>
+                        <div className="input-field col s12">
+                            <label>주소</label>
+                            <input name="address" type="text" className="validate" placeholder="주소주소"/>
+                        </div>
+                        <div className="input-field col s12">
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridState">
+                                    <Form.Label>국가</Form.Label>
+                                    <Form.Control as="select" value="Choose...">
+                                        <option>-</option>
+                                        <option>대한민국</option>
+                                    </Form.Control>
+                                </Form.Group>
+                            </Form.Row>
+                        </div>
+                        
+                        {/* 담딩투어 경험 Y/N */}
+                        <Form.Group id="formGridCheckbox">
+                            <Form.Check type="checkbox" label="담딩투어 경험이 있으시면 체크해주세요. "/>
+                        </Form.Group>
+                        
+                        <a className="waves-effect waves-light btn">회원가입</a>
+                    </div>
+                </div>
+            </div>
+        );
 
         return (
             <div className="container auth">
--- src/container/Signup.js
+++ src/container/Signup.js
@@ -18,63 +18,6 @@ class Signup extends Component {
 
                 <Authentication mode={false} />
 
-                <div style={{ marginTop: "100px", marginRight: "100px", marginLeft: "100px" }}>
-                    <div style={{ marginTop: "100px", marginRight: "300px", marginLeft: "300px" }}>
-                        {/* login popup */}
-                        {/* 이름, 아이디, (중복확인) 패스워드, 패스워드확인, 생년월일, 성별, 이메일 (중복확인), 휴대폰 번호(본인확인), 주소, 가입 계기, 담딩투어 경험자/비경험 */}
-                        <Form>
-                            {/* 이름 */}
-                            <Form.Row>
-                                <Form.Group as={Col} controlId="formGridName">
-                                    <Form.Label>이름</Form.Label>
-                                    <Form.Control type="name" placeholder="이름" />
-                                </Form.Group>
-                            </Form.Row>
-                            {/*아이디*/}
-                            <Form.Group controlId="formGridId">
-                                <Form.Label>아이디</Form.Label>
-                                <Form.Control placeholder="아이디" />
-                            </Form.Group>
-
-                            <Form.Group controlId="formGridPassword">
-                                <Form.Label>패스워드</Form.Label>
-                                <Form.Control placeholder="비밀번호를 입력해주세요. " />
-                            </Form.Group>
-
-                            <Form.Group controlId="formGridPassword">
-                                <Form.Label>패스워드 확인 </Form.Label>
-                                <Form.Control placeholder="비밀번호를 재입력해주세요. " />
-                            </Form.Group>
-
-                            <Form.Row>
-                                <Form.Group as={Col} controlId="formGridCity">
-                                    <Form.Label>주소</Form.Label>
-                                    <Form.Control />
-                                </Form.Group>
-                            </Form.Row>
-
-                            <Form.Row>
-                                <Form.Group as={Col} controlId="formGridState">
-                                    <Form.Label>국가</Form.Label>
-                                    <Form.Control as="select" value="Choose...">
-                                        <option>-</option>
-                                        <option>대한민국</option>
-                                    </Form.Control>
-                                </Form.Group>
-
-
-                            </Form.Row>
-
-                            <Form.Group id="formGridCheckbox">
-                                <Form.Check type="checkbox" label="담딩투어 경험이 있으시면 체크해주세요. " />
-                            </Form.Group>
-
-                            <Button variant="primary" type="submit" onSubmit={this.activeSignup}>
-                                가입
-                            </Button>
-                        </Form>
-                    </div>
-                </div>
                 <Footer/>
             </div>
         )
