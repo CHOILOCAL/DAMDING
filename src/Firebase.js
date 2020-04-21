f2a131b3fe8d083dff33f3fe61b9a411d0cca199
Choilocal <choihyunji1103@gmail.com>
2020-04-21 17:22:57 +0900

    파이어베이스 호스팅 연결(커스텀도메인x), 파이어베이스 설정(firebase.js)

---
+++ src/Firebase.js
@@ -0,0 +1,24 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+
+const settings = {
+    timestampsInSnapshots: true
+};
+
+const config = {
+    apiKey: "AIzaSyB4FQSUq5yueyaIDpqV4yuqmmtn17w-4M0",
+    authDomain: "damding-42450.firebaseapp.com",
+    databaseURL: "https://damding-42450.firebaseio.com",
+    projectId: "damding-42450",
+    storageBucket: "damding-42450.appspot.com",
+    messagingSenderId: "35981381532",
+    appId: "1:35981381532:web:40df41e3c6151c95e28259",
+    measurementId: "G-RVRD2P7N9T"
+};
+firebase.initializeApp(config);
+
+firebase
+    .firestore()
+    .settings(settings);
+
+export default firebase;
